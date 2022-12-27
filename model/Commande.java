ae39d9e12ea43410958e1a88be788bbc7e434d7f
A-make-cloud <68588824+A-make-cloud@users.noreply.github.com>
2022-12-27 13:53:04 +0100

    Update Commande.java
    Signed-off-by: A-make-cloud <68588824+A-make-cloud@users.noreply.github.com>

--- model/Commande.java
+++ model/Commande.java
@@ -11,7 +11,7 @@ public class Commande {
 	private String trackingNbr;
 	
 	private int id_Client;
-	private int id_ClienTest;
+	private int id_ClienTestNumero2;
 	private String adresse;
 	private String telephone;
 	private String email;
